7b31165d1185a1acda32317661ba75f86b7141e2
Aryaman Pandey <130132988+aryamanpandu@users.noreply.github.com>
2024-01-08 21:49:53 -0400

    Completed Advent Of Code part 2

--- Main.java
+++ Main.java
@@ -14,9 +14,8 @@ public class Main {
             Scanner in = new Scanner(file);
 
             while (in.hasNextLine()) {
-                String intermediate1 = replaceCharsToDigits(in.nextLine());
+                String intermediate1 = replaceCharsToDigitsOrdered(in.nextLine());
                 String intermediate2 = findFirstandLastDigit(intermediate1);
-                System.out.println(intermediate2);
                 calibrationList.add(Integer.parseInt(intermediate2));
             }
 
@@ -86,41 +85,65 @@ public class Main {
     public static String replaceCharsToDigits(String value) {
         String result = "";
         if (value.contains("one")) {
-            result = value.replace("one", "1");
+            result = value.replace("one", "o1e");
             value = result;
         }
         if (value.contains("two")) {
-            result = value.replace("two", "2");
+            result = value.replace("two", "t2o");
             value = result;
         }
         if (value.contains("three")) {
-            result = value.replace("three", "3");
+            result = value.replace("three", "t3e");
             value = result;
         }
         if (value.contains("four")) {
-            result = value.replace("four", "4");
+            result = value.replace("four", "f4r");
             value = result;
         }
         if (value.contains("five")) {
-            result = value.replace("five", "5");
+            result = value.replace("five", "f5e");
             value = result;
         }
         if (value.contains("six")) {
-            result = value.replace("six", "6");
+            result = value.replace("six", "s6x");
             value = result;
         }
         if (value.contains("seven")) {
-            result = value.replace("seven", "7");
+            result = value.replace("seven", "s7n");
             value = result;
         }
         if (value.contains("eight")) {
-            result = value.replace("eight", "8");
+            result = value.replace("eight", "e8t");
             value = result;
         }
         if (value.contains("nine")) {
-            result = value.replace("nine", "9");
+            result = value.replace("nine", "n9e");
             value = result;
         }
         return result;
+    }
+
+    /**
+     * This method is used to check if the String provided contains the digits in alphabetical form
+     * of one two three four
+     * five six seven eight and nine and if it does, the method replaces it
+     * with their digit number in the string in the order they come in.
+     * @param value the string provided
+     * @return the resultant string which contains the digits and not their alphabetical form
+     */
+    public static String replaceCharsToDigitsOrdered(String value) {
+        String[] digits = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //This array is used to check the alphabetical numbers
+        String[] valuesToPut = {"o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e"}; //This is the value to be replaced.
+        String currentWord = ""; //used to store the current word which also becomes the result by the end of the nested for loops
+
+        for (int i = 0; i < value.length(); i++) {
+            currentWord += value.charAt(i);
+            for (int j = 0; j < digits.length; j++) {
+                if(currentWord.contains(digits[j])) {
+                    currentWord = currentWord.replaceFirst(digits[j], valuesToPut[j]);
+                }
+            }
+        }
+        return currentWord;
     }
 }
